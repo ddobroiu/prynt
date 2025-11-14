9538c6376012b5ec84a0b949b652a3318f1ebf99
ddobroiu <d_dobroiu@YAHOO.COM>
2025-11-14 23:42:36 +0200

    2

---
+++ prisma/migrations/20251114_234226_add_awb_and_status/migration.sql
@@ -0,0 +1,6 @@
+﻿-- AlterTable
+ALTER TABLE "Order" ADD COLUMN     "awbCarrier" TEXT,
+ADD COLUMN     "awbNumber" TEXT,
+ADD COLUMN     "canceledAt" TIMESTAMP(3),
+ADD COLUMN     "status" TEXT NOT NULL DEFAULT 'active';
+
