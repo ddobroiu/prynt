a2c3afadc377e3bb693faf0c72d91ea76150082d
ddobroiu <d_dobroiu@YAHOO.COM>
2025-11-14 23:45:17 +0200

    2

--- prisma/migrations/20251114_234226_add_awb_and_status/migration.sql
+++ prisma/migrations/20251114_234226_add_awb_and_status/migration.sql
@@ -1,6 +1,11 @@
-﻿-- AlterTable
-ALTER TABLE "Order" ADD COLUMN     "awbCarrier" TEXT,
+﻿ALTER TABLE "Order" ADD COLUMN     "awbCarrier" TEXT,
 ADD COLUMN     "awbNumber" TEXT,
 ADD COLUMN     "canceledAt" TIMESTAMP(3),
 ADD COLUMN     "status" TEXT NOT NULL DEFAULT 'active';
+
+-- AlterTable
+ALTER TABLE "Order" ADD COLUMN "awbCarrier" TEXT,
+ADD COLUMN "awbNumber" TEXT,
+ADD COLUMN "canceledAt" TIMESTAMP(3),
+ADD COLUMN "status" TEXT NOT NULL DEFAULT 'active';
 
