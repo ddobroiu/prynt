3c4fbc1499ec65e12f08e54ce09b1474177e4177
ddobroiu <d_dobroiu@YAHOO.COM>
2025-11-16 22:17:59 +0200

    s

---
+++ types/jsonwebtoken.d.ts
@@ -0,0 +1,7 @@
+declare module 'jsonwebtoken' {
+  export function sign(payload: any, secret: string, options?: any): string;
+  export function verify(token: string, secret: string, options?: any): any;
+  export function decode(token: string, options?: any): any;
+  const _default: any;
+  export default _default;
+}
