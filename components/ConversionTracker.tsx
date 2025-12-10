a673546a6b420667db451dcaca7bf2f307f91eda
ddobroiu <d_dobroiu@YAHOO.COM>
2025-12-10 20:32:14 +0200

    a

--- components/ConversionTracker.tsx
+++ components/ConversionTracker.tsx
@@ -2,6 +2,12 @@
 
 import { useEffect } from "react";
 
+declare global {
+  interface Window {
+    dataLayer?: any[];
+  }
+}
+
 export default function ConversionTracker({ orderNo }: { orderNo: number | null }) {
   useEffect(() => {
     if (!orderNo) return;
